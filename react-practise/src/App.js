2966af21b4db14c4e14e2f9a977462c2843ff1e9
nazar zaheer <73424320+2041373@users.noreply.github.com>
2023-04-07 18:29:19 +0100

    States and Props completed
    I need to go over states and props

--- react-practise/src/App.js
+++ react-practise/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Greetings from './components/Greetings';
+import States from './components/States';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
       <Greetings/>
+      <States />
     </div>
   );
 }
--- react-practise/src/components/States.js
+++ react-practise/src/components/States.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react'
 
+
 export default class States extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      counter: 0,
+    }
+  }
+
   render() {
+
+  const  increment = () => {
+      this.setState({counter:this.state.counter +1})
+    }
+
+  const  decrement = () => {
+       this.setState({counter:this.state.counter -1})
+    }
+
     return (
-      <div>States</div>
+      <div>
+
+      <button onClick={() => increment()}> + </button>
+
+      <h1>{this.state.counter}</h1>
+
+      <button onClick={() => decrement()}> - </button>
+
+
+
+      </div>
     )
   }
 }
